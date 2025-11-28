e19d4d2067adf1e4a85db32f0d06ee16d986f6bd
PMH <ghgh5663@naver.com>
2025-11-28 21:59:23 +0900

    아직 페이지간 연결 불안정, 프로토타입

--- frontend/src/pages/ArrivalPage.tsx
+++ frontend/src/pages/ArrivalPage.tsx
@@ -35,7 +35,7 @@ export default function ArrivalPage() {
 
   const handleConfirm = () => {
     if (state.recognizedLabel) {
-      navigate("/triptype", {
+      navigate("/passenger", {
         state: {
           departureStation: departureStation, // 출발역 전달
           arrivalStation: state.recognizedLabel, // 도착역 전달
